995c3e37253d0ee0c963a2b9c07f28bb4765bc2a
Benard Muinde <92430769+BenardMN@users.noreply.github.com>
2022-06-30 17:09:49 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
-* Description: main - prints out a string.
-* return : 0 if succcess.
+*Description: main - prints out a string.
+*Return : 0 if succcess.
 */
 int main(void)
 {
