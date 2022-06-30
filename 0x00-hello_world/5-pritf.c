307627ce3359549fe5c350d9ef26f9191e2f46a5
Benard Muinde <benardmunuve934@gmail.com>
2022-06-30 06:25:31 -0700

    feat

---
+++ 0x00-hello_world/5-pritf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ *Description: prints out a string
+ *Return : 0 if succcess
+ */
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return 0;
+}
