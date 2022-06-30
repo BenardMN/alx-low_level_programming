08467362d65e61fffd8b9bc16386853a9f0f8a00
Benard Muinde <benardmunuve934@gmail.com>
2022-06-30 06:11:09 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- *desc: prints out a string
- *return : 0 if succes
+
+ * Description: main - Prints out a string to stdout.
+
+ * Return: 0 if success.
+
  */
+
 int main(void)
+  
 {
+  
   puts("\"Programming is like building a multilingual puzzle");
-  return 0;
+  
+  return (0);
+  
 }
