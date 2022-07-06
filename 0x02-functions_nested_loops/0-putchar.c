da87b71105b968be6f7172ffd4e582b8676adc3b
Benard Muinde <benardmunuve934@gmail.com>
2022-07-06 05:44:38 -0700

    feat

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *prints out a string followed by a new line
+ *return - 0 always success
+ **/
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
